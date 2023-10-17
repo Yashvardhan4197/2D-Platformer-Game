da5e7070fbb08a9637a4d61cd40904f637227537
Yashvardhan4197 <yashvardhan4197@gmail.com>
2023-10-17 22:48:03 +0530

    Added Movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,7 +1,9 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using JetBrains.Annotations;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.UIElements;
 
@@ -9,57 +11,79 @@ public class PlayerController : MonoBehaviour
 {
     
     public Animator animator;
+    public float Speed;
     public BoxCollider2D boxCollider;
+    public int jump;
+
+    private Rigidbody2D rb;
     void Start()
     {
+        rb=gameObject.GetComponent<Rigidbody2D>();
     }
     void Update()
     {
-        float speed=Input.GetAxisRaw("Horizontal");
-        animator.SetFloat("Speed",Mathf.Abs(speed));
+        float Horizontal=Input.GetAxisRaw("Horizontal");
+        float Vertical=Input.GetAxisRaw("Jump");
+        Player_Anim(Horizontal,Vertical);
+        Player_Transform(Horizontal,Vertical);
+        //Crouch while pressing ctrl
+        bool crouch=Input.GetKey(KeyCode.LeftControl);
+        Player_Crouch(crouch);
+
+        
+
+    }
+
+    private void Player_Transform(float Horizontal, float Vertical)
+    {
+        //Player Movement-
+        Vector3 position=transform.position;
+        position.x=position.x+Horizontal*Speed*Time.deltaTime;
+        transform.position=position;
+
+    }
+
+
+    private void Player_Anim(float Horizontal,float Vertical)
+    {
+        animator.SetFloat("Speed",Mathf.Abs(Horizontal));
         Vector3 scale=transform.localScale;
-        if(speed<0)
+        if(Horizontal<0)
         {
             if(scale.x>0)
             {
             scale.x=-1f*scale.x;
             }
-        }else if(speed>0)
+        }
+        else if(Horizontal>0)
         {
             scale.x=Mathf.Abs(scale.x);
         }
         transform.localScale=scale;
 
-        //Crouch while pressing ctrl
-        bool crouch=Input.GetKey(KeyCode.LeftControl);
-        bool jump=false;
-        float jc=Input.GetAxisRaw("Vertical");
-        if(crouch==true||jc<0){
-            if(jc<0)
-            {
-                crouch=true;
-            }
-            animator.SetBool("Crouch",crouch);
+
+//Jump-Animation
+        if(Vertical>0)
+        {
+        animator.SetBool("Jump",true);
+        }
+        if(Vertical==0)
+        {
+            animator.SetBool("Jump",false);
+        }
+    }
+    void Player_Crouch(bool crouch)
+    {
+        if(crouch==true){
             boxCollider.size =new Vector2(1.04f,1.505f);
             boxCollider.offset=new Vector2(-0.117f,0.604f);
-        }else if(crouch==false){
+            animator.SetBool("Crouch",crouch);
+        }
+        else if(crouch==false){
             animator.SetBool("Crouch",crouch);
             boxCollider.size =new Vector2(0.92f,2.35f);
             boxCollider.offset=new Vector2(0.012f,1.03f);
         }
-
-        if(jc>0)
-        {
-            jump=true;
-            animator.SetBool("Jump",jump);
-        }
-        if(jc<=0)
-        {
-            jump=false;
-            animator.SetBool("Jump",jump);
-        }
-
-        
-
     }
+   
 }
