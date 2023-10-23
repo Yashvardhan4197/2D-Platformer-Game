a9c4b0ea3e990088fd89390554dcbf425ff8fd20
Yashvardhan4197 <yashvardhan4197@gmail.com>
2023-10-23 19:30:39 +0530

    Added Rotating Animations

--- Assets/Scripts/KeyController.cs
+++ Assets/Scripts/KeyController.cs
@@ -4,12 +4,15 @@ using UnityEngine;
 
 public class KeyController : MonoBehaviour
 {
+    [SerializeField]Animator anim;
     private void OnCollisionEnter2D(Collision2D other)
     {
         if(other.gameObject.GetComponent<PlayerController>()!=null){
             PlayerController playerController=other.gameObject.GetComponent<PlayerController>();
             playerController.pickup();
-            Destroy(gameObject);
+            //play animation
+            anim.SetBool("Picked?",true);
+            Destroy(gameObject,0.15f);
         }
     }
 }
